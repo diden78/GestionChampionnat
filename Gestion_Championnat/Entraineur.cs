03618880634e767859c782d745f35619ee82478e
diden78 <diden78@gmail.com>
2015-03-21 15:22:37 +0100

    Update Entraineur.cs

--- Gestion_Championnat/Entraineur.cs
+++ Gestion_Championnat/Entraineur.cs
@@ -10,6 +10,7 @@ namespace Gestion_Championnat
     {
         public Entraineur(string nom) : base(nom)
         {    
+            //commentaire
         }
 
         public Entraineur(string nom, string prenom) : base(nom, prenom)
